0772651a1e1efadf6a28d84169de0542478b9c18
isaacs <i@izs.me>
2011-10-27 11:42:39 -0700

    Print data about extended headers

--- examples/reader.js
+++ examples/reader.js
@@ -3,6 +3,12 @@ var tar = require("../tar.js")
 
 fs.createReadStream(__dirname + "/../test/fixtures/c.tar")
   .pipe(tar.Reader())
+  .on("extendedHeader", function (e) {
+    console.error("extended pax header", e.props)
+    e.on("end", function () {
+      console.error("extended pax fields:", e.fields)
+    })
+  })
   .on("entry", function (e) {
     console.error("entry", e.props)
     e.on("data", function (c) {
