eb09b0209da2461a21d2cc33f9cc0da4fd1503fa
isaacs <i@izs.me>
2011-10-28 18:00:00 -0700

    handle metadata files properly when they're stacked on top of one another

--- examples/reader.js
+++ examples/reader.js
@@ -9,6 +9,15 @@ fs.createReadStream(__dirname + "/../test/fixtures/c.tar")
       console.error("extended pax fields:", e.fields)
     })
   })
+  .on("ignoredEntry", function (e) {
+    console.error("ignoredEntry?!?", e.props)
+  })
+  .on("longLinkpath", function (e) {
+    console.error("longLinkpath entry", e.props)
+    e.on("end", function () {
+      console.error("value=%j", e.body.toString())
+    })
+  })
   .on("longPath", function (e) {
     console.error("longPath entry", e.props)
     e.on("end", function () {
