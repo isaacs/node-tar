d17d5d905cfcd00f74f5962c213dab165408a6b9
isaacs <i@izs.me>
2011-11-10 22:40:16 -0800

    test one file packed into a tarball

--- test/pack.js
+++ test/pack.js
@@ -6,17 +6,17 @@ var tap = require("tap")
   , Reader = fstream.Reader
   , Writer = fstream.Writer
   , path = require("path")
-  , dir = path.resolve(__dirname, "fixtures")
-  , target = path.resolve(__dirname, "pack.tar")
+  , input = path.resolve(__dirname, "fixtures/omega.txt")
+  , target = path.resolve(__dirname, "tmp/pack.tar")
+
+process.on("uncaughtException", function (er) {
+  console.error(er.stack)
+  console.error(er)
+})
 
 tap.test("make a tar", function (t) {
   // put the package.json in as a global header, for kicks.
-  var reader = Reader({ path: dir
-                      , filter: function () {
-                          return !this.path.match(/\.tar$/)
-                               && !this.path.match(/\.hex$/)
-                        }
-                      })
+  var reader = Reader(input)
   var pack = Pack(pkg)
   var writer = Writer(target)
 
@@ -24,7 +24,20 @@ tap.test("make a tar", function (t) {
   t.ok(pack, "pack ok")
   t.ok(writer, "writer ok")
 
-  reader.pipe(pack).pipe(writer)
+  pack.pipe(writer)
+
+  var parse = tar.Parse()
+  pack.on("data", parse.write.bind(parse))
+  pack.on("end", parse.end.bind(parse))
+  parse.on("*", function (ev, e) {
+    console.error("      entry %s", ev, e.props)
+  })
+
+
+  // reader.pipe(pack)
+  pack.add(reader)
+  pack.end()
+
   writer.on("close", function () {
     t.ok(true, "it finished!")
     t.end()
