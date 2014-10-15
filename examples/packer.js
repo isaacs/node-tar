14d0196ebbf24aea5e7e3bd3c99e6fddaf2b2ccf
Dmitry Shirokov <deadrunk@gmail.com>
2014-10-15 12:09:45 +1100

    better error handling

--- examples/packer.js
+++ examples/packer.js
@@ -4,7 +4,21 @@ var tar = require("../tar.js")
 
 var dir_destination = fs.createWriteStream('dir.tar')
 
+
+var on_error = function(err) {
+  console.error('An error occurred:', err)
+}
+
+var on_end = function() {
+  console.log('Packed!')
+}
+
+var packer = tar.Pack({ noProprietary: true })
+  .on('error', on_error)
+  .on('end', on_end);
+
 // This must be a "directory"
 fstream.Reader({ path: __dirname, type: "Directory" })
-  .pipe(tar.Pack({ noProprietary: true }))
+  .on('error', on_error)
+  .pipe(packer)
   .pipe(dir_destination)
