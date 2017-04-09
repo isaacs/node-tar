ab86f8bc5007813d6a82476689f6b62e98b97817
isaacs <i@izs.me>
2017-04-09 14:13:43 -0700

    load all the things! Makes coverage fail (on purpose)

---
+++ test/load-all.js
@@ -0,0 +1,8 @@
+// just load all the files so we can't cheat coverage by avoiding something
+const fs = require('fs')
+const path = require('path')
+const lib = path.resolve(__dirname, '../lib')
+fs.readdirSync(lib)
+  .filter(f => /\.js$/.test(f))
+  .forEach(f => require('../lib/' + f))
+require('tap').pass('all lib files loaded')
