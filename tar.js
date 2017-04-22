d44048e8959b678f672cfd06c515481e6ed133e4
isaacs <i@izs.me>
2017-04-21 20:54:18 -0700

    stub files for top-level API surface
    Also, this makes `npm test` into a nice todo-list.

--- tar.js
+++ tar.js
@@ -1,4 +1,19 @@
+'use strict'
 
-// exports.Pack = require("./lib/pack.js")
-exports.parse = require("./lib/parse.js")
-exports.extract = require("./lib/extract.js")
+// classes
+exports.Field = require('./lib/field.js')
+exports.Header = require('./lib/header.js')
+exports.Pack = require('./lib/pack.js')
+exports.Parse = require('./lib/parse.js')
+exports.Pax = require('./lib/pax.js')
+exports.ReadEntry = require('./lib/read-entry.js')
+exports.types = require('./lib/types.js')
+exports.Unpack = require('./lib/unpack.js')
+exports.WriteEntry = require('./lib/write-entry.js')
+
+// high-level commands
+exports.x = exports.extract = require('./lib/extract.js')
+exports.c = exports.create = require('./lib/create.js')
+exports.t = exports.list = require('./lib/list.js')
+exports.u = exports.update = require('./lib/update.js')
+exports.r = exports.replace = require('./lib/replace.js')
