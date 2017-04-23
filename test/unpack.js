df3ebab036111d852b6fb74404211166f877bde1
isaacs <i@izs.me>
2017-04-22 21:53:34 -0700

    start at unpack test

---
+++ test/unpack.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const Unpack = require('../lib/unpack.js')
+const UnpackSync = Unpack.Sync
+const t = require('tap')
+
+const fs = require('fs')
+const path = require('path')
+const fixtures = path.resolve(__dirname, 'fixtures')
+const files = path.resolve(fixtures, 'files')
+const tars = path.resolve(fixtures, 'tars')
+const unpackdir = path.resolve(fixtures, 'unpack')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+
+// t.teardown(_ => rimraf.sync(unpackdir))
+
+t.test('setup', t => {
+  rimraf.sync(unpackdir)
+  mkdirp.sync(unpackdir)
+  t.end()
+})
+
+t.test('basic file unpack tests', t => {
+  const basedir = path.resolve(unpackdir, 'basic')
+  // t.teardown(_ => rimraf.sync(basedir))
+
+  const cases = {
+    'emptypax.tar': {
+      '🌟.txt': '🌟✧✩⭐︎✪✫✬✭✮⚝✯✰✵✶✷✸✹❂⭑⭒★☆✡☪✴︎✦✡️🔯✴️🌠\n',
+      'one-byte.txt': 'a'
+    },
+    'body-byte-counts.tar': {
+      '1024-bytes.txt': new Array(1024).join('x') + '\n',
+      '512-bytes.txt': new Array(512).join('x') + '\n',
+      'one-byte.txt': 'a',
+      'zero-byte.txt': ''
+    },
+    'utf8.tar': {
+      '🌟.txt': '🌟✧✩⭐︎✪✫✬✭✮⚝✯✰✵✶✷✸✹❂⭑⭒★☆✡☪✴︎✦✡️🔯✴️🌠\n',
+      'Ω.txt': 'Ω',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/Ω.txt': 'Ω'
+    },
+    'file.tar': {
+      'one-byte.txt': 'a'
+    },
+    'global-header.tar': {
+      'one-byte.txt': 'a'
+    },
+    'long-pax.tar': {
+      '120-byte-filename-cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc'
+    },
+    'long-paths.tar': {
+      '100-byte-filename-cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      '120-byte-filename-cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      '170-byte-filename-cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/a.txt': 'short\n',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc': 'cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc',
+      'long-path/r/e/a/l/l/y/-/d/e/e/p/-/f/o/l/d/e/r/-/p/a/t/h/Ω.txt': 'Ω'
+    }
+  }
+
+  const tarfiles = Object.keys(cases)
+  t.plan(tarfiles.length)
+  t.jobs = tarfiles.length
+
+  tarfiles.forEach(tarfile => {
+    t.test(tarfile, t => {
+      const tf = path.resolve(tars, tarfile)
+      const dir = path.resolve(basedir, tarfile)
+      t.beforeEach(cb => {
+        // rimraf.sync(dir)
+        mkdirp.sync(dir)
+        cb()
+      })
+
+      const check = t => {
+        const expect = cases[tarfile]
+        Object.keys(expect).forEach(file => {
+          const f = path.resolve(dir, file)
+          t.equal(fs.readFileSync(f, 'utf8'), expect[file], file)
+        })
+        t.end()
+      }
+
+      t.plan(2)
+
+      t.test('async unpack', t => {
+        t.plan(2)
+        t.test('strict', t => {
+          const unpack = new Unpack({ cwd: dir, strict: true })
+          fs.createReadStream(tf).pipe(unpack)
+          unpack.on('end', _ => setTimeout(_ => check(t), 500))
+        })
+        t.test('loose', t => {
+          const unpack = new Unpack({ cwd: dir })
+          fs.createReadStream(tf).pipe(unpack)
+          // unpack.on('end', _ => check(t))
+          unpack.on('end', _ => setTimeout(_ => check(t), 500))
+        })
+      })
+
+      t.test('sync unpack', t => {
+        t.plan(2)
+        t.test('strict', t => {
+          const unpack = new UnpackSync({ cwd: dir })
+          unpack.end(fs.readFileSync(tf))
+          check(t)
+        })
+        t.test('loose', t => {
+          const unpack = new UnpackSync({ cwd: dir })
+          unpack.end(fs.readFileSync(tf))
+          check(t)
+        })
+      })
+    })
+  })
+})
+
+t.test('cwd default to process cwd', t => {
+  const u = new Unpack()
+  const us = new UnpackSync()
+  const cwd = process.cwd()
+  t.equal(u.cwd, cwd)
+  t.equal(us.cwd, cwd)
+  t.end()
+})
