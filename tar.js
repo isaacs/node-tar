d53b1f2181c35272f47dca6cf0fa22568d34ae72
isaacs <i@izs.me>
2017-04-27 15:16:21 -0700

    trivial little style cleanups

--- tar.js
+++ tar.js
@@ -1,19 +1,19 @@
 'use strict'
 
-// classes
-exports.Field = require('./lib/field.js')
-exports.Header = require('./lib/header.js')
-exports.Pack = require('./lib/pack.js')
-exports.Parse = require('./lib/parse.js')
-exports.Pax = require('./lib/pax.js')
-exports.ReadEntry = require('./lib/read-entry.js')
-exports.types = require('./lib/types.js')
-exports.Unpack = require('./lib/unpack.js')
-exports.WriteEntry = require('./lib/write-entry.js')
-
 // high-level commands
 exports.c = exports.create = require('./lib/create.js')
 exports.r = exports.replace = require('./lib/replace.js')
 exports.t = exports.list = require('./lib/list.js')
 exports.u = exports.update = require('./lib/update.js')
 exports.x = exports.extract = require('./lib/extract.js')
+
+// classes
+exports.Pack = require('./lib/pack.js')
+exports.Unpack = require('./lib/unpack.js')
+exports.Parse = require('./lib/parse.js')
+exports.ReadEntry = require('./lib/read-entry.js')
+exports.WriteEntry = require('./lib/write-entry.js')
+exports.Header = require('./lib/header.js')
+exports.Pax = require('./lib/pax.js')
+exports.Field = require('./lib/field.js')
+exports.types = require('./lib/types.js')
