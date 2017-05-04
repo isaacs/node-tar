6e9cadbf7ab15d0a916308a2c4ff72b764f041e8
isaacs <i@izs.me>
2017-05-04 00:08:59 -0700

    Refactoring to attempt to improve performance

--- benchmarks/parse/tar-stream.js
+++ benchmarks/parse/tar-stream.js
@@ -3,6 +3,12 @@ const path = require('path')
 const file = process.argv[2] || path.resolve(__dirname, '../npm.tar')
 const fs = require('fs')
 
+let slices = 0
+Buffer.prototype.slice = (original => function () {
+  slices ++
+  return original.apply(this, arguments)
+})(Buffer.prototype.slice)
+
 const start = process.hrtime()
 const p = tar.extract()
 p.on('entry', (entry, stream, callback) => {
@@ -13,5 +19,6 @@ p.on('data', _ => _)
 process.on('exit', (code, signal) => {
   const end = process.hrtime(start)
   console.log(end[0]*1e3 + end[1]/1e6)
+  console.log(slices)
 })
 fs.createReadStream(file).pipe(p)
