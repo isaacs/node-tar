5892512821ad925e27aab6eabb7db7fab4d6cb95
isaacs <i@izs.me>
2017-05-04 00:09:36 -0700

    benchmark scripts

--- benchmarks/parse/tar-stream.js
+++ benchmarks/parse/tar-stream.js
@@ -3,12 +3,6 @@ const path = require('path')
 const file = process.argv[2] || path.resolve(__dirname, '../npm.tar')
 const fs = require('fs')
 
-let slices = 0
-Buffer.prototype.slice = (original => function () {
-  slices ++
-  return original.apply(this, arguments)
-})(Buffer.prototype.slice)
-
 const start = process.hrtime()
 const p = tar.extract()
 p.on('entry', (entry, stream, callback) => {
@@ -19,6 +13,5 @@ p.on('data', _ => _)
 process.on('exit', (code, signal) => {
   const end = process.hrtime(start)
   console.log(end[0]*1e3 + end[1]/1e6)
-  console.log(slices)
 })
 fs.createReadStream(file).pipe(p)
