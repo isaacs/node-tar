3c0988bb8fb48b9132746fd290e13f25038a13a7
isaacs <i@izs.me>
2017-05-04 22:09:22 -0700

    benchmark old node-tar parser

---
+++ benchmarks/parse/old-sync.js
@@ -0,0 +1,14 @@
+const Parse = require('tar').Parse
+const path = require('path')
+const file = process.argv[2] || path.resolve(__dirname, '../npm.tar')
+const fs = require('fs')
+const data = fs.readFileSync(file)
+
+const start = process.hrtime()
+const p = new Parse()
+p.on('entry', entry => entry.resume())
+p.on('end', _ => {
+  const end = process.hrtime(start)
+  console.error(end[0]*1e3 + end[1]/1e6)
+})
+p.end(data)
