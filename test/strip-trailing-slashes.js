adf35115dbc6e7a479fe3c38f859baf6ffff7a7a
isaacs <i@izs.me>
2021-08-03 14:26:08 -0700

    Avoid an unlikely but theoretically possible redos
    When stripping the trailing slash from `files` arguments, we were using
    `f.replace(/\/+$/, '')`, which can get exponentially slow when `f`
    contains many `/` characters.
    
    Tested a few variants and found one that's faster than the regexp
    replacement for short strings, long strings, and long strings containing
    many instances of repeated `/` characters.
    
    This is "unlikely but theoretically possible" because it requires that
    the user is passing untrusted input into the `tar.extract()` or
    `tar.list()` array of entries to parse/extract, which would be quite
    unusual.

---
+++ test/strip-trailing-slashes.js
@@ -0,0 +1,7 @@
+const t = require('tap')
+const stripSlash = require('../lib/strip-trailing-slashes.js')
+const short = '///a///b///c///'
+const long = short.repeat(10) + '/'.repeat(1000000)
+
+t.equal(stripSlash(short), '///a///b///c')
+t.equal(stripSlash(long), short.repeat(9) + '///a///b///c')
