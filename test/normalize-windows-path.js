ab3e9036dc4504413fb6f154ea6943af5c187e26
isaacs <i@izs.me>
2021-08-04 15:48:21 -0700

    fix: normalize paths on Windows systems
    This change uses / as the One True Path Separator, as the gods of POSIX
    intended in their divine wisdom.
    
    On windows, \ characters are converted to /, everywhere and in depth.
    However, on posix systems, \ is a valid filename character, and is not
    treated specially.  So, instead of splitting on `/[/\\]/`, we can now
    just split on `'/'` to get a set of path parts.
    
    This does mean that archives with entries containing \ will extract
    differently on Windows systems than on correct systems.  However, this
    is also the behavior of both bsdtar and gnutar, so it seems appropriate
    to follow suit.
    
    Additionally, dirCache pruning is now done case-insensitively.  On
    case-sensitive systems, this potentially results in a few extra lstat
    calls.  However, on case-insensitive systems, it prevents incorrect
    cache hits.

---
+++ test/normalize-windows-path.js
@@ -0,0 +1,27 @@
+const t = require('tap')
+
+const realPlatform = process.platform
+const fakePlatform = realPlatform === 'win32' ? 'posix' : 'win32'
+const requireInject = require('require-inject')
+
+t.test('posix', t => {
+  if (realPlatform === 'win32')
+    process.env.TESTING_TAR_FAKE_PLATFORM = fakePlatform
+  else
+    delete process.env.TESTING_TAR_FAKE_PLATFORM
+  const normPath = requireInject('../lib/normalize-windows-path.js')
+  t.equal(normPath('/some/path/back\\slashes'), '/some/path/back\\slashes')
+  t.equal(normPath('c:\\foo\\bar'), 'c:\\foo\\bar')
+  t.end()
+})
+
+t.test('win32', t => {
+  if (realPlatform !== 'win32')
+    process.env.TESTING_TAR_FAKE_PLATFORM = fakePlatform
+  else
+    delete process.env.TESTING_TAR_FAKE_PLATFORM
+  const normPath = requireInject('../lib/normalize-windows-path.js')
+  t.equal(normPath('/some/path/back\\slashes'), '/some/path/back/slashes')
+  t.equal(normPath('c:\\foo\\bar'), 'c:/foo/bar')
+  t.end()
+})
