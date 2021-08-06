53cea6ed26a7f1a0b420b374e4219e328b9cf067
isaacs <i@izs.me>
2021-08-05 23:21:22 -0700

    tests: run (and pass) on windows
    Threw a kludge in there to run tap with lower coverage reqs on Windows.

---
+++ test/fixtures/test.js
@@ -0,0 +1,20 @@
+const platform = process.platform === 'win32' ? 'win32' : 'posix'
+const {spawn} = require('child_process')
+const c = spawn(process.execPath, [
+  process.env.npm_execpath,
+  'run',
+  `test:${platform}`,
+  '--',
+  ...process.argv.slice(2),
+], {
+  stdio: 'inherit',
+})
+c.on('close', (code, signal) => {
+  process.exitCode = code
+  if (signal) {
+    process.kill(process.pid, signal)
+    setTimeout(() => {}, 200)
+  }
+})
+process.on('SIGTERM', () => c.kill('SIGTERM'))
+process.on('SIGINT', () => c.kill('SIGINT'))
