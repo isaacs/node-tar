4501bdbe59fb56dbc0de6e7e220340aaaef9394d
isaacs <i@izs.me>
2023-05-16 10:33:22 -0700

    Normalize unicode internally using NFD
    Previously, the path reservation system, which defends against unicode
    path name collisions (the subject of a handful of past CVE issues), was
    using NFKD normalization internally to determine of two paths would be
    likely to reference the same file on disk.
    
    This has the weird effect of normalizing things like `℀` into simple
    decomposed character strings, for example `a/c`. These can contain
    slashes and double-dot sections, which means that the path reservations
    may end up reserving more (or different) paths than intended.
    
    Thankfully, tar was already *extracting* properly, even if the path
    reservations collided, and these collisions resulted in tar being *more*
    aggressive than it should be in restricting parallel extraction, rather
    than less. That's a good direction to err in, for security, but also,
    made tar less efficient than it could be in some edge cases.
    
    Using NFD normalization, unicode characters are not decomposed in
    compatibility mode, but still result in matching path reservation keys
    as intended.
    
    This does not cause any change in observed behavior, other than allowing
    some files to be extracted in parallel where it is provably safe to do
    so.
    
    Credit: discovered by @Sim4n6. This did not result in a juicy security
    vulnerability, but it sure looked like one at first. They were extremely
    patient, thorough, and persistent in trying to pin this down to a POC
    and CVE. There is very little reward or visibility when a security
    researcher finds a bug that doesn't result in a security disclosure, but
    the attempt often results in improvements to the project.

--- test/normalize-unicode.js
+++ test/normalize-unicode.js
@@ -1,5 +1,8 @@
+process.env.TESTING_TAR_FAKE_PLATFORM = 'win32'
 const t = require('tap')
 const normalize = require('../lib/normalize-unicode.js')
+const stripSlash = require('../lib/strip-trailing-slashes.js')
+const normPath = require('../lib/normalize-windows-path.js')
 
 // café
 const cafe1 = Buffer.from([0x63, 0x61, 0x66, 0xc3, 0xa9]).toString()
@@ -10,3 +13,27 @@ const cafe2 = Buffer.from([0x63, 0x61, 0x66, 0x65, 0xcc, 0x81]).toString()
 t.equal(normalize(cafe1), normalize(cafe2), 'matching unicodes')
 t.equal(normalize(cafe1), normalize(cafe2), 'cached')
 t.equal(normalize('foo'), 'foo', 'non-unicode string')
+
+t.test('normalize with strip slashes', t => {
+  const paths = [
+    '\\a\\b\\c\\d\\',
+    '﹨aaaa﹨dddd﹨',
+    '＼bbb＼eee＼',
+    '＼＼＼＼＼eee＼＼＼＼＼＼',
+    '¼foo.txt',
+    '1/4foo.txt',
+  ]
+
+  t.plan(paths.length)
+
+  for (const path of paths) {
+    t.test(JSON.stringify(path), t => {
+      const a = normalize(stripSlash(normPath(path)))
+      const b = stripSlash(normPath(normalize(path)))
+      t.matchSnapshot(a, 'normalized')
+      t.equal(a, b, 'order should not matter')
+      t.end()
+    })
+  }
+  t.end()
+})
